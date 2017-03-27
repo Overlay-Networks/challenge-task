a5ed619525ba3f8dd4936c5aa75114ce24b59bc1
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-03-27 22:23:14 +0200

    fix message controller

--- src/main/java/com/uzh/csg/overlaynetworks/web/controller/MessageController.java
+++ src/main/java/com/uzh/csg/overlaynetworks/web/controller/MessageController.java
@@ -1,6 +1,5 @@
 package com.uzh.csg.overlaynetworks.web.controller;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/message")
 public class MessageController {
 
-	@RequestMapping(
-		method = GET,
-		produces = APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(method = GET)
 	public String getMessage() {
 		return "message";
 	}
