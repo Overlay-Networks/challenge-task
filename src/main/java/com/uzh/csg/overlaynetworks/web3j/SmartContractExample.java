93d45a40378cc9f8bdba4d11767be95f55ec72a9
Tschonny <Tschonny@DESKTOP-INAQ6TV>
2017-04-13 18:37:16 +0200

    First try to integrate smart contracts into the whole thing. It is atm just the standard "greeter" contract from the tutorial

---
+++ src/main/java/com/uzh/csg/overlaynetworks/web3j/SmartContractExample.java
@@ -0,0 +1,36 @@
+package com.uzh.csg.overlaynetworks.web3j;
+
+
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.Utf8String;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.Contract;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.util.concurrent.Future;
+
+public class SmartContractExample {
+	Credentials credentials = CredentialsHelper.getDefaultCredentials();
+	Web3j web3 = Web3j.build(new HttpService());
+	String message = "TEST";
+	
+	/*
+	 * At the moment i did not get the Web3 command line interface to run, which is necessary to create
+	 * the Contracts class for some reason. Will be included asap.
+	 */
+	BigInteger gasPrice = new BigInteger("10000");
+	BigInteger gasLimit = new BigInteger("10000000");
+	BigInteger initialValue = new BigInteger("3000000");
+	Utf8String myGreeting = new Utf8String("hallo");
+	
+
+	Future<ExampleContract> myContract = ExampleContract.deploy(web3, credentials, gasPrice, gasLimit, initialValue, myGreeting);
+	
+	
+
+	
+}
