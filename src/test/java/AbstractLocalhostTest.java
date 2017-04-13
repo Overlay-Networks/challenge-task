54f9656217ded0f326ae35c19ae243651e1b9867
christiankiller <christian.killer@uzh.ch>
2017-04-13 09:38:44 +0200

    JUnit test GetGethVersionExample & abstract test class

---
+++ src/test/java/AbstractLocalhostTest.java
@@ -0,0 +1,31 @@
+
+// import com.hshnordbank.web3j.CredentialsHelper;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+
+import com.uzh.csg.overlaynetworks.web3j.CredentialsHelper;
+import com.uzh.csg.overlaynetworks.web3j.Example;
+
+import java.math.BigInteger;
+
+import static org.web3j.tx.Contract.GAS_LIMIT;
+import static org.web3j.tx.ManagedTransaction.GAS_PRICE;
+
+abstract class AbstractLocalhostTest {
+    
+    private static final BigInteger MAX_GAS_PER_TX = BigInteger.valueOf(3141592L);
+
+    // every test has its own web3j instance
+    private final Web3j web3j = Web3j.build(new HttpService("http://localhost:8545"));
+
+    protected <T> T run(Example<T> example) {
+        final Credentials credentials = CredentialsHelper.getDefaultCredentials();
+        return example.run(web3j, credentials, GAS_PRICE, GAS_LIMIT, MAX_GAS_PER_TX);
+    }
+
+    protected Web3j getWeb3j() {
+        return web3j;
+    }
+
+}
