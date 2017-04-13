7b5e4546d5853d1cd38c601ee052b357e50950b5
christiankiller <christian.killer@uzh.ch>
2017-04-13 09:39:08 +0200

    Example Connection classes

---
+++ src/main/java/com/uzh/csg/overlaynetworks/web3j/GetGethVersionExample.java
@@ -0,0 +1,27 @@
+package com.uzh.csg.overlaynetworks.web3j;
+
+import com.uzh.csg.overlaynetworks.web3j.AsyncHelper;
+
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+
+import org.web3j.protocol.core.Request;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+
+import java.math.BigInteger;
+
+/**
+ * Created by Christian Killer on 12.04.2017
+ */
+
+public class GetGethVersionExample implements Example<String> {
+	
+     @Override
+    public String run(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit, BigInteger gasValue) {
+        final Request<?, Web3ClientVersion> asyncRequest = web3j.web3ClientVersion();
+        final Web3ClientVersion result = AsyncHelper.waitForResult(asyncRequest);
+        return result.getWeb3ClientVersion();
+    }
+
+}
+
