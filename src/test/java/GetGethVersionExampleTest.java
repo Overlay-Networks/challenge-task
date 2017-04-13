e4b2a981b9a8dc92b7dadcf22bdfd60ba5f1c9bd
christiankiller <christian.killer@uzh.ch>
2017-04-13 09:38:44 +0200

    JUnit test GetGethVersionExample & abstract test class

---
+++ src/test/java/GetGethVersionExampleTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import com.uzh.csg.overlaynetworks.web3j.GetGethVersionExample;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GetGethVersionExampleTest extends AbstractLocalhostTest {
+
+    @Test
+    public void shouldRetrieveTheCorrectBlockchainVersion() {
+        final String version = run(new GetGethVersionExample());
+        
+        // Depends on your individual installation
+        assertThat(version).isEqualTo("Geth/v1.5.9-stable-a07539fb/darwin/go1.8");
+    }
+
+} 
