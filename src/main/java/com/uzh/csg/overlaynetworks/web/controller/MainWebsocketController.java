5cdf4ce4f32086395af8749d41d7b08414f8f418
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-04-15 17:39:59 +0200

    remove example code

--- src/main/java/com/uzh/csg/overlaynetworks/web/controller/MainWebsocketController.java
+++ src/main/java/com/uzh/csg/overlaynetworks/web/controller/MainWebsocketController.java
@@ -2,27 +2,15 @@ package com.uzh.csg.overlaynetworks.web.controller;
 
 import java.util.Set;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.uzh.csg.overlaynetworks.domain.dto.ContactWithStatus;
 import com.uzh.csg.overlaynetworks.domain.dto.ReceiveMessage;
 import com.uzh.csg.overlaynetworks.domain.dto.ReceiveNotary;
-import com.uzh.csg.overlaynetworks.web.dto.example.Greeting;
-import com.uzh.csg.overlaynetworks.web.dto.example.HelloMessage;
 
 @Controller
 public class MainWebsocketController {
-
-	/* EXAMPLE */
-	@MessageMapping("/hello") // input
-	@SendTo("/topic/greetings") // output
-	public Greeting greeting(HelloMessage message) throws Exception {
-		Thread.sleep(1000); // simulated delay
-		return new Greeting("Hello, " + message.getName() + "!");
-	}
-	/* EXAMPLE END */
 
 	@SendTo("/topic/receive-message")
 	public ReceiveMessage receiveMessage(ReceiveMessage message) {
