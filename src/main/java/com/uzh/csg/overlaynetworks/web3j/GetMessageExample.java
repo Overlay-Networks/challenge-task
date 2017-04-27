2785fb810fbd7bba77464597579907705c1ce713
Tschonny <Tschonny@DESKTOP-INAQ6TV>
2017-04-27 10:26:13 +0200

    Push for readability

---
+++ src/main/java/com/uzh/csg/overlaynetworks/web3j/GetMessageExample.java
@@ -0,0 +1,30 @@
+package com.uzh.csg.overlaynetworks.web3j;
+
+import java.math.BigInteger;
+
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Utf8String;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+
+import com.uzh.csg.overlaynetworks.web3j.AsyncHelper;
+
+public class GetMessageExample implements Example<String> {
+	
+
+	@Override
+	public String run(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit,
+			BigInteger gasValue) {
+		
+		BigInteger bigInt = new BigInteger("20");
+		Utf8String message = new Utf8String("JavaTest");
+		Address receiver = new Address("0x2480e0e351128f46392bFCe00d02764b31c79970");
+		Uint256 identifier = new Uint256(bigInt);
+		GetMessage myContract = GetMessage.load("0xaEF3f82C5B9e9366A6169d71fccD64293c0f534E", web3j, credentials, gasPrice, gasLimit);
+		
+		myContract.save(message , receiver , identifier);
+		
+		return myContract.getMessage(identifier).toString();
+	}
+}
