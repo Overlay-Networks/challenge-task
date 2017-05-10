07ad9e1ae0d61dad5d20582a2dcb4ba8f6664054
Tschonny <Tschonny@DESKTOP-INAQ6TV>
2017-05-10 16:52:49 +0200

    Test should now access Blockchain
    Not sure tho

--- src/test/java/IsContractDeployedExampleTest.java
+++ src/test/java/IsContractDeployedExampleTest.java
@@ -1,5 +1,6 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import org.junit.Test;
@@ -13,11 +14,11 @@ import com.uzh.csg.overlaynetworks.wrappers.MessageRegistry;
 public class IsContractDeployedExampleTest extends AbstractLocalhostTest{
 
     @Test
-    public void shouldReturnTrueIfContractIsDeployed() {
+    public void shouldReturnTrueIfContractIsDeployed() throws InterruptedException, ExecutionException {
         final String contractAddress = run(new IsContractDeployedExample());
         final MessageRegistry messageRegistry = MessageRegistryHelper.getMessageRegistry(getWeb3j(), contractAddress); 
-        final Future<Bool> asyncResult = messageRegistry.isDeployed();
-        final boolean isDeployed = AsyncHelper.waitForResult(asyncResult).getValue();
-        assertThat(isDeployed).isTrue();
+        final Bool asyncResult = messageRegistry.isDeployed().get();
+        
+        assert asyncResult.getValue();
     }
 }
