a9219fa6a167657dbbeb5abcce4ac968d614f0b8
christiankiller <christian.killer@uzh.ch>
2017-05-12 17:26:26 +0200

    Add Logging; Debugging Test Cases

--- src/test/java/IsContractDeployedExampleTest.java
+++ src/test/java/IsContractDeployedExampleTest.java
@@ -2,6 +2,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.Test;
 import org.web3j.abi.datatypes.Bool;
@@ -12,13 +14,23 @@ import com.uzh.csg.overlaynetworks.web3j.MessageRegistryHelper;
 import com.uzh.csg.overlaynetworks.wrappers.MessageRegistry;
 
 public class IsContractDeployedExampleTest extends AbstractLocalhostTest{
+	
+	private static final Logger LOGGER = Logger.getLogger(CanGetValuesTest.class.getName() );
 
     @Test
     public void shouldReturnTrueIfContractIsDeployed() throws InterruptedException, ExecutionException {
-        final String contractAddress = run(new IsContractDeployedExample());
+    	
+    	try{
+    	final String contractAddress = run(new IsContractDeployedExample());
         final MessageRegistry messageRegistry = MessageRegistryHelper.getMessageRegistry(getWeb3j(), contractAddress); 
         final Bool asyncResult = messageRegistry.isDeployed().get();
         
-        assert asyncResult.getValue();
+		LOGGER.log(Level.INFO, "[INFO]"+this.getClass().toString()+"[messageRegistry.isDeployed="+asyncResult.getValue().toString()+"]");
+        assertThat(asyncResult.getValue()).isTrue();
+        
+    	} catch(Exception e){
+    		LOGGER.log(Level.INFO, "[INFO]["+e.toString()+"]["+e.getClass().toString()+"]");
+    	}
+
     }
 }
