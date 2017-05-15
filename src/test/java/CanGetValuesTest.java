43f7183da3f0bc32cce23facfb57edf2e38507db
christiankiller <christian.killer@uzh.ch>
2017-05-15 15:35:59 +0200

    caffeine break

--- src/test/java/CanGetValuesTest.java
+++ src/test/java/CanGetValuesTest.java
@@ -1,16 +1,10 @@
 import static org.assertj.core.api.Assertions.assertThat;
-
 import java.math.BigInteger;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
 import org.junit.Test;
-import org.web3j.abi.datatypes.Bool;
 import org.web3j.abi.datatypes.Type;
 import org.web3j.abi.datatypes.generated.Uint256;
-
-import com.uzh.csg.overlaynetworks.web3j.AsyncHelper;
 import com.uzh.csg.overlaynetworks.web3j.IsContractDeployedExample;
 import com.uzh.csg.overlaynetworks.web3j.MessageRegistryHelper;
 import com.uzh.csg.overlaynetworks.wrappers.MessageRegistry;
@@ -36,15 +30,16 @@ private static final Logger LOGGER = Logger.getLogger(CanGetValuesTest.class.get
         
         final List<Type> message = messageRegistry.getMessage(myIdentifier).get();
         
-		LOGGER.log(Level.INFO,this.getClass().toString()+"[MESSAGE.size()="+message.size()+"]");
+		LOGGER.log(Level.INFO,this.getClass().toString()+"message.size()="+message.size());
+		LOGGER.log(Level.INFO,this.getClass().toString()+"message.toString()="+message.toString());
 		
         assertThat(message).isNotNull();
+        assertThat(message.size()).isEqualTo(4);
     	assertThat(message.toString()).isEqualTo("test");
     	
     	} catch(Exception e){
     		LOGGER.log(Level.INFO, e.toString()+"["+e.getClass().toString()+"]");
     	}
     	
-  
     }
 }
--- src/test/java/WriteValuesToChainTest.java
+++ src/test/java/WriteValuesToChainTest.java
@@ -4,6 +4,8 @@ import java.math.BigInteger;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.Test;
 import org.web3j.abi.datatypes.Address;
@@ -20,18 +22,28 @@ import com.uzh.csg.overlaynetworks.wrappers.MessageRegistry;
 
 public class WriteValuesToChainTest extends AbstractLocalhostTest{
 
+	private static final Logger LOGGER = Logger.getLogger(WriteValuesToChainTest.class.getName() );
+
     @Test
     public void shouldReturnTrueIfWrittenToBlockchain() throws InterruptedException, ExecutionException {
         final String contractAddress = run(new IsContractDeployedExample());
+        
+		LOGGER.log(Level.INFO,this.getClass().toString()+"contractAddress="+contractAddress+"]");
+        
         final MessageRegistry messageRegistry = MessageRegistryHelper.getMessageRegistry(getWeb3j(), contractAddress); 
+        
+        // Write to Blockchain
         final BigInteger myBigInt = new BigInteger("1250"); 
         final Uint256 identifier = new Uint256(myBigInt);
-        final Utf8String message = new Utf8String("This is the java Test");
+        final Utf8String message = new Utf8String("This is the Java Test");
         final Address receiver = new Address("0x5BbB245A661C4C112AA0e848A2BB007Ba3e9B628");
   
         TransactionReceipt transactionReceipt = messageRegistry.save(message, receiver, identifier).get();
         
-        System.out.println(transactionReceipt.getTransactionHash());
+		LOGGER.log(Level.INFO,this.getClass().toString()+"transactionReceipt.getTransactionHash="+transactionReceipt.getTransactionHash());
         
+    	
+
+		
     }
 }
