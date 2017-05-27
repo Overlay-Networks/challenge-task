e1c182a17404c61b3e29571daaf9df76af679c89
YuryB <yurybelevskiy@gmail.com>
2017-05-27 19:58:01 +0200

    Re-wrote P2PClientTest to make it non-unit test and attempted to bootstrap 20 clients together

--- src/test/java/P2PClientTest.java
+++ src/test/java/P2PClientTest.java
@@ -7,10 +7,6 @@ import com.uzh.csg.overlaynetworks.p2p.P2PClient;
 import com.uzh.csg.overlaynetworks.p2p.P2PClientDelegate;
 import com.uzh.csg.overlaynetworks.p2p.PeerInfo;
 import com.uzh.csg.overlaynetworks.p2p.error.P2PError;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,45 +16,33 @@ import java.util.List;
  */
 public class P2PClientTest implements P2PClientDelegate {
 
-	private static List<P2PClient> clients = new ArrayList<>();
+	private List<P2PClient> clients;
 
-	private static final int NUM_CLIENTS = 20;
-
-	@BeforeClass
-	public static void setUp() {
-		for(int i=0; i < NUM_CLIENTS; i++) {
-			P2PClient client = new P2PClient("peer_" + i);
-			clients.add(client);
+	public P2PClientTest(List<P2PClient> clients) {
+		this.clients = clients;
+		for (P2PClient client : this.clients) {
+			client.delegate = this;
 		}
 	}
 
-	@AfterClass
-	public static void tearDown() {
-
-	}
-
 	/* Assumes that bootstrapping server is running and P2PClient is configured with correct IP and port */
-	@Test
 	public void bootstrapClients() {
 		for(P2PClient client : clients) {
-			client.delegate = this;
 			try {
 				client.start();
 			} catch (LoginFailedException lfe) {
-				assertThat(false).isTrue();
+				System.err.println("Failed to start client " + client);
 				lfe.printStackTrace();
 			}
 		}
 	}
 
-	@Test
-	public void sendDirectMessageBetweenAllClients() {
-
-	}
-	
 	public void didLogin(PeerInfo peer, P2PError error) throws LoginFailedException {
-		assertThat(error == null).isTrue();
-		System.out.println("DID LOGIN: " + peer.getUsername());
+		if(error != null) {
+			System.err.println("Failed to login client " + peer.getUsername() + "! Error is: " + error.getErrorMessage());
+		} else {
+			System.out.println("DID LOGIN: " + peer.getUsername());
+		}
 	}
 
 	public void didReceiveMessage(Message message, Contact from, MessageResult result, P2PError error) {
---
+++ src/test/java/P2PClientTestStart.java
@@ -0,0 +1,24 @@
+import com.uzh.csg.overlaynetworks.p2p.P2PClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yurybelevskiy on 27.05.17.
+ */
+public class P2PClientTestStart {
+
+	private static List<P2PClient> clients = new ArrayList<>();
+
+	private static final int NUM_CLIENTS = 20;
+
+	public static void main(String[] args) {
+		for(int i=0; i < NUM_CLIENTS; i++) {
+			P2PClient client = new P2PClient("peer_" + i);
+			clients.add(client);
+		}
+		P2PClientTest p2pTest = new P2PClientTest(clients);
+		p2pTest.bootstrapClients();
+	}
+
+}
